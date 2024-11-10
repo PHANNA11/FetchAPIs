9014a4f0e72cbb9e7b9c766cecae368bdbd6919b
PHA PHANNA <phaphanna2018@gmail.com>
2024-11-10 10:23:31 +0700

    Shrimmer & cached Image + StateMixin

--- lib/controller/product_controller.dart
+++ lib/controller/product_controller.dart
@@ -5,14 +5,34 @@ import 'package:api_integration_app/resource/api_resources.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:http/http.dart' as http;
 
-class ProductController extends GetxController {
-  Future<List<ProductModel>> getProduct() async {
+class ProductController extends GetxController
+    with StateMixin<List<ProductModel>> {
+  @override
+  void onInit() {
+    super.onInit();
+    change([], status: RxStatus.success());
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+    getProduct();
+  }
+
+  Future getProduct() async {
+    if (status.isLoadingMore) return;
+    if (!status.isLoadingMore && value!.isNotEmpty) {
+      change(value, status: RxStatus.loadingMore());
+    } else {
+      change([], status: RxStatus.loading());
+    }
     try {
       var response = await http.get(Uri.parse(APIRoute().products));
       if (response.statusCode == 200) {
-        return productFromJson(response.body);
+        value?.addAll(productFromJson(response.body));
+        return change(value, status: RxStatus.success());
       } else {
-        log(response.body.toString());
+        return change([], status: RxStatus.empty());
       }
     } catch (e) {
       log(e.toString());
--- lib/view/home_store.dart
+++ lib/view/home_store.dart
@@ -1,106 +1,156 @@
 import 'package:api_integration_app/controller/product_controller.dart';
-import 'package:api_integration_app/model/product_model.dart';
 import 'package:api_integration_app/view/detail_product.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
+import 'package:shimmer/shimmer.dart';
 
-class HomeStore extends StatefulWidget {
-  const HomeStore({super.key});
-
-  @override
-  State<HomeStore> createState() => _HomeStoreState();
-}
-
-class _HomeStoreState extends State<HomeStore> {
-  List<ProductModel> listProducts = [];
-  void getData() async {
-    await ProductController().getProduct().then((value) {
-      setState(() {
-        listProducts = value;
-      });
-    });
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    getData();
-  }
-
+class HomeStore extends GetView<ProductController> {
+  HomeStore({super.key});
+  final controller = Get.put(ProductController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Store'),
       ),
-      body: GridView.builder(
+      body: controller.obx(
+          (state) => GridView.builder(
+                shrinkWrap: true,
+                //  physics: NeverScrollableScrollPhysics(),
+                itemCount: state!.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  mainAxisSpacing: 4,
+                  crossAxisSpacing: 4,
+                  crossAxisCount: 2,
+                  mainAxisExtent: 250,
+                ),
+                itemBuilder: (context, index) => Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: GestureDetector(
+                        onTap: () {
+                          Get.to(() => DetailProductScreen(
+                                product: state[index],
+                              ));
+                        },
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                child: Hero(
+                                  tag: '${state[index].id}',
+                                  child: CachedNetworkImage(
+                                    imageUrl: state[index].image.toString(),
+                                    imageBuilder: (context, imageProvider) =>
+                                        Container(
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                          image: imageProvider,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                    placeholder: (context, url) =>
+                                        productShrimmerSingle(),
+                                    errorWidget: (context, url, error) =>
+                                        Icon(Icons.error),
+                                  ),
+
+                                  // Container(
+                                  //   decoration: BoxDecoration(
+                                  //       borderRadius: BorderRadius.circular(10),
+                                  //       image: DecorationImage(
+                                  //           fit: BoxFit.cover,
+                                  //           image: NetworkImage(state[index]
+                                  //               .image
+                                  //               .toString()))),
+                                  // ),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 4),
+                                child: Text(
+                                  state[index].title.toString(),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 4),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      '\$ ${state[index].price}',
+                                      style: const TextStyle(
+                                          color: Colors.red,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      'view : ${state[index].rating!.count}',
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ))),
+              ),
+          onEmpty: const Center(
+            child: Text('Empty'),
+          ),
+          onLoading: productShrimmer()),
+    );
+  }
+
+  Widget productShrimmer() {
+    return GridView.builder(
         shrinkWrap: true,
         //  physics: NeverScrollableScrollPhysics(),
-        itemCount: listProducts.length,
+        itemCount: 10,
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           mainAxisSpacing: 4,
           crossAxisSpacing: 4,
           crossAxisCount: 2,
           mainAxisExtent: 250,
         ),
-        itemBuilder: (context, index) => Padding(
-            padding: const EdgeInsets.all(4),
-            child: GestureDetector(
-                onTap: () {
-                  Get.to(() => DetailProductScreen(
-                        product: listProducts[index],
-                      ));
-                },
-                child: DecoratedBox(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        child: Hero(
-                          tag: '${listProducts[index].id}',
-                          child: Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image: NetworkImage(
-                                        listProducts[index].image.toString()))),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 4),
-                        child: Text(
-                          listProducts[index].title.toString(),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 4),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              '\$ ${listProducts[index].price}',
-                              style: TextStyle(
-                                  color: Colors.red,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              'view : ${listProducts[index].rating!.count}',
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ))),
-      ),
-    );
+        itemBuilder: (context, index) => Shimmer.fromColors(
+              period: const Duration(milliseconds: 2500),
+              baseColor: Colors.blueGrey,
+              highlightColor: Colors.grey,
+              child: Container(
+                margin: const EdgeInsets.all(4),
+                width: 200,
+                height: 150,
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+            ));
+  }
+
+  Widget productShrimmerSingle() {
+    return Shimmer.fromColors(
+        period: const Duration(milliseconds: 2500),
+        baseColor: Colors.blueGrey,
+        highlightColor: Colors.grey,
+        child: Container(
+          margin: const EdgeInsets.all(4),
+          width: 180,
+          height: 150,
+          decoration: ShapeDecoration(
+            color: Colors.white,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          ),
+        ));
   }
 }
